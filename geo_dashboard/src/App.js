41ae3309abc64f48fe3c29d4bfe4e612ba9c74a3
Aman Kaif <aman220202@gmail.com>
2023-03-22 12:32:54 +0530

    Initialised RnD geo_dashboard

---
+++ geo_dashboard/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Chungus
+      </header>
+
+      <div className='Main'>
+        <h1 className="Title">
+            GeoSpatial Data Dashboard
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
